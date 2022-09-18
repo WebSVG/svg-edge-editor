8830044c63fe3bfdb984345bd121eaf437757dec
wass <wassim.filali@gmail.com>
2022-09-18 11:34:05 +0200

    added deno to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: deno()
 });
